f217247ec1591c5e01b0796823bd64b1b519a3ba
Wouter Kenis <Da.wouter@gmail.com>
2018-11-04 15:05:28 +0100

    Updating records work.

--- CustomerManagement/MainWindow.xaml.cs
+++ CustomerManagement/MainWindow.xaml.cs
@@ -24,14 +24,15 @@ namespace CustomerManagement
     public partial class MainWindow : Window
     {
         List<Customer> customers;
+        string connectionString;
         public MainWindow()
         {
             InitializeComponent();
 
-            string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;
+            connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;
             AttachDbFilename=C:\Users\Gebruiker\source\repos\CustomerManagement\CustomerManagement\Customers.mdf;Integrated Security=True";
 
-            SqlConnection connection = new SqlConnection(ConnectionString);
+            SqlConnection connection = new SqlConnection(connectionString);
 
             SqlCommand command = new SqlCommand();
             command.Connection = connection;
@@ -73,6 +74,45 @@ namespace CustomerManagement
         private void saveButton_Click(object sender, RoutedEventArgs e)
         {
             //update db, list, combobox
+
+            string updateQry = "UPDATE Customers SET " +
+                               "FirstName = @Firstname" + ", " +
+                               "LastName = @Lastname" + ", " +
+                               "Address = @Address" + ", " +
+                               "Country = @Country" + ", " +
+                               "Phonenumber = @Phonenumber" + ", " +
+                               "Email = @Email" + " " +
+                               "WHERE Id = @Id";
+
+            SqlConnection connectionUpdate = new SqlConnection(connectionString);
+
+            SqlCommand commandUpdate = new SqlCommand();
+            commandUpdate.Connection = connectionUpdate;
+            commandUpdate.CommandText = updateQry;
+
+            connectionUpdate.Open();
+
+            commandUpdate.Parameters.AddWithValue("@Id", allCustomersComboBox.SelectedIndex + 1);
+            commandUpdate.Parameters.AddWithValue("@FirstName", firstNameBox.Text.Trim());
+            commandUpdate.Parameters.AddWithValue("@LastName", lastNameBox.Text.Trim());
+            commandUpdate.Parameters.AddWithValue("@Address", addressBox.Text.Trim());
+            commandUpdate.Parameters.AddWithValue("@Country", countryBox.Text.Trim());
+            commandUpdate.Parameters.AddWithValue("@Phonenumber", phoneNumberBox.Text.Trim());
+            commandUpdate.Parameters.AddWithValue("@Email", emailBox.Text.Trim());
+            try
+            {
+                commandUpdate.ExecuteNonQuery();
+                MessageBox.Show("Updated.");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Not Updated.");
+            }
+            finally
+            {
+                connectionUpdate.Close();
+            }
+
         }
         private void allCustomersComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
